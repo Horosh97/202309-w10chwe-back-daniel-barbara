a1e5b008685debf660c8484ac7ebf3dd1096305e
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-21 18:33:40 +0100

    Add user types and model

---
+++ src/features/users/model/User.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+import type { UserStructure } from "../types";
+
+const userSchema = new Schema<UserStructure>({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    minlength: 4,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model("User", userSchema, "users");
+
+export default User;
